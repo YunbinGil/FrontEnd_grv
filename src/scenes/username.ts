38ee919312e6babbfdd7c879c92eddfdbe904d4f
MinboyKim <kdm3801@gmail.com>
2024-05-25 16:39:05 +0900

    feat: button style & form hidden
    간단한 버튼 스타일링 추가 & 채팅 폼 가리기

--- src/scenes/username.ts
+++ src/scenes/username.ts
@@ -1,55 +1,62 @@
-import { USERNAME, TYPE_MAIN} from "@constants/scenes";
+import { CHAT } from "@constants/actions";
+import { USERNAME, TYPE_MAIN } from "@constants/scenes";
 import { Scene } from "phaser";
 
 class Username extends Scene {
-    private inputElement!: HTMLInputElement;
-    private buttonElement!: HTMLButtonElement;
+  private inputElement!: HTMLInputElement;
+  private buttonElement!: HTMLButtonElement;
+  private formElement!: HTMLElement;
 
-    constructor() {
-        super({key : USERNAME, active : true});
+  constructor() {
+    super({ key: USERNAME, active: true });
+    this.formElement = document.getElementById(CHAT)!;
+  }
+
+  preload() {
+    this.load.image("logo", "logo.png");
+    this.formElement.classList.add("hidden");
+  }
+
+  create() {
+    const logo = this.add.image(400, 300, "logo");
+    logo.setScale(0.5);
+    if (this.inputElement) {
+      this.inputElement.remove();
+    }
+    if (this.buttonElement) {
+      this.buttonElement.remove();
     }
 
-    preload() {
-      this.load.image('logo', 'logo.png');
-    }
+    this.inputElement = document.createElement("input");
+    this.inputElement.type = "text";
+    this.inputElement.placeholder = "Enter your name";
+    this.inputElement.style.position = "absolute";
+    this.inputElement.style.top = "55%";
+    this.inputElement.style.left = "50%";
+    this.inputElement.style.transform = "translate(-50%, -50%)";
+    this.inputElement.style.zIndex = "1";
+    document.body.appendChild(this.inputElement);
 
-    create() {
-      const logo = this.add.image(400,300, 'logo');
-      logo.setScale(0.5);
-      if (this.inputElement) {
-        this.inputElement.remove();
-      }
-      if (this.buttonElement) {
-          this.buttonElement.remove();
-      }
+    this.buttonElement = document.createElement("button");
+    this.buttonElement.textContent = "확인";
+    this.buttonElement.style.position = "absolute";
+    this.buttonElement.style.top = "65%";
+    this.buttonElement.style.left = "50%";
+    this.buttonElement.style.transform = "translate(-50%, -50%)";
+    this.buttonElement.style.zIndex = "1";
+    this.buttonElement.style.padding = "10px 20px";
+    this.buttonElement.style.borderRadius = "10px";
+    document.body.appendChild(this.buttonElement);
 
-      this.inputElement = document.createElement('input');
-      this.inputElement.type = 'text';
-      this.inputElement.placeholder = 'Enter your name';
-      this.inputElement.style.position = 'absolute';
-      this.inputElement.style.top = '50%';
-      this.inputElement.style.left = '50%';
-      this.inputElement.style.transform = 'translate(-50%, -50%)';
-      this.inputElement.style.zIndex = '1';
-      document.body.appendChild(this.inputElement);
-
-      this.buttonElement = document.createElement('button');
-      this.buttonElement.textContent = '확인';
-      this.buttonElement.style.position = 'absolute';
-      this.buttonElement.style.top = '60%';
-      this.buttonElement.style.left = '50%';
-      this.buttonElement.style.transform = 'translate(-50%, -50%)';
-      this.buttonElement.style.zIndex = '1';
-      document.body.appendChild(this.buttonElement);
-
-      this.buttonElement.addEventListener('click', () => {
-          const username = this.inputElement.value;
-          localStorage.setItem('username', username);
-          this.inputElement.classList.add('hidden');
-          this.buttonElement.classList.add('hidden');
-          this.scene.start(TYPE_MAIN);
-        });
-    }
+    this.buttonElement.addEventListener("click", () => {
+      const username = this.inputElement.value;
+      localStorage.setItem("username", username);
+      this.inputElement.classList.add("hidden");
+      this.buttonElement.classList.add("hidden");
+      this.formElement.classList.remove("hidden");
+      this.scene.start(TYPE_MAIN);
+    });
+  }
 }
 
 export default Username;
